affac460c548c64d5d9170000ab87f4e46ad20eb
Vamshi Reddy <vamshiproject02@gmail.com>
2024-08-27 01:34:00 +0530

    first commit
    Signed-off-by: Vamshi Reddy <vamshiproject02@gmail.com>

--- wordl-spin/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ResponseBuilder } from "@fermyon/spin-sdk";
-
-export async function handler(req: Request, res: ResponseBuilder) {
-    console.log(req);
-    res.send("hello universe");
-}
